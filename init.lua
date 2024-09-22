8c1c2934b1f847a9941e2a788168b6feb3c84c8f
beatmil <anutorn@gmail.com>
2024-09-22 16:23:08 +0100

    Correctly indent highligh yank hunk XD

--- init.lua
+++ init.lua
@@ -425,12 +425,12 @@ end
 
 -- highlight yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', {}),
-  desc = 'Hightlight selection on yank',
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 10 }
-  end,
+	group = vim.api.nvim_create_augroup('highlight_yank', {}),
+	desc = 'Hightlight selection on yank',
+	pattern = '*',
+	callback = function()
+		vim.highlight.on_yank { higroup = 'IncSearch', timeout = 10 }
+	end,
 })
 
 
